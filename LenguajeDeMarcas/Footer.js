7db8bc0b2deb9e8cde517c6130ac568dfd11f98f
gaizka9 <129118955+gaizka9@users.noreply.github.com>
2023-03-31 14:45:45 +0200

    Antes De Semana Santa

---
+++ LenguajeDeMarcas/Footer.js
@@ -0,0 +1,37 @@
+multilineString =
+'<footer class="bg-dark text-center text-white">' +
+    '<div class="container p-4">' +
+        '<div class="row">' +
+            '<div class="offset-3 col-3">' +
+                '<img src="Logo.png" alt="MDN">' +
+            '</div>' +
+            '<div class="col-3 align-self-center">' +
+                '<section class="mb-4">' +
+                    '<a class="btn btn-outline-light btn-floating m-1" href="https://es-es.facebook.com/eDreams.ES/" role="button">' +
+                        '<i class="fa fa-facebook"></i>' +
+                    '</a>' +
+
+                    '<a class="btn btn-outline-light btn-floating m-1" href="https://twitter.com/eDreams?ref_src=twsrc%5Egoogle%7Ctwcamp%5Eserp%7Ctwgr%5Eauthor" role="button">' +
+                        '<i class="fa fa-twitter"></i>' +
+                    '</a>' +
+
+                    '<a class="btn btn-outline-light btn-floating m-1" href="https://www.edreams.es/" role="button">' +
+                        '<i class="fa fa-google"></i>' +
+                    '</a>' +
+
+                    '<a class="btn btn-outline-light btn-floating m-1" href="https://www.instagram.com/edreams/?hl=es" role="button">' +
+                        '<i class="fa fa-instagram"></i>' +
+                    '</a>' +
+
+                    '<a class="btn btn-outline-light btn-floating m-1" href="https://github.com/JonLinazaPetralanda/AgenciaViajesGRUPO2_1AW3" role="button">' +
+                        '<i class="fa fa-github"></i>' +
+                    '</a>' +
+
+                '</section >' +
+                '<section class="mb-4"></section>' +
+                '<a class="text-white"> &copy; 2023 MML BIDAIAK | Eskubide guztiak erreserbatuak</a>' + 
+            '</div>' +
+        '</div >' +
+    '</div>' +
+'</footer >' ;
+document.write(multilineString);
---
+++ LenguajeDeMarcas/navBar.js
@@ -0,0 +1,23 @@
+multilineString =
+'<nav class="navbar navbar-expand-sm bg-dark navbar-dark">' +
+  '<div class="container-fluid">' +
+    '<ul class="navbar-nav">' +
+      '<li class="nav-item">' +
+        '<img src="Logo.png" alt="..." height="36">' +
+      '</li>' +
+      '<li class="nav-item">' +
+        '<a class="nav-link active">MML BIDAIAK</a>' +
+      '</li>' +
+      '<li class="nav-item">' +
+        '<a class="nav-link" href="index.html">Hasiera</a>' +
+      '</li>' +
+      '<li class="nav-item">' +
+        '<a class="nav-link" href="Hegaldiak.html">Hegaldiak</a>' +
+      '</li>' +
+      '<li class="nav-item">' +
+        '<a class="nav-link" href="Kontaktua.html">Kontaktua</a>' +
+      '</li>' +
+    '</ul>' +
+  '</div>' +
+'</nav>' ;
+document.write(multilineString);
